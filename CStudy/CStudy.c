b6c9abaae34dc7c69f001041a3e44c59b493343a
enrhd24 <enrhd567@gmail.com>
2023-04-21 21:39:30 +0900

    CStudy C_language sytax updated

--- CStudy/CStudy.c
+++ CStudy/CStudy.c
@@ -32,13 +32,15 @@ int main(){
     int integer, variable, sum = 0;
 
     for(int i = 0; i < 6; i++){
-        printf("학생 %d 성적을 입력하세요 : ", i+1);
+        printf("student %d score : ", i+1);
         scanf("%d", &score[i]);
     }
     for(int i = 0; i < 6; i++){
-        printf("합계: %d\n", sum);
-        printf("평균 %.2f\n", (double)sum/6);
-        printf("\n score[6]에 저장되어있는 숫자:\n");
+        sum += score[i];
+        
+        printf("Sum: %d\n", sum);
+        printf("Average %.2f\n", (double)sum/6);
+        printf("\n score[6] saved:\n");
     }
     for(int v = 0; v < 6; v++){
         printf(" %d",score[v]);
