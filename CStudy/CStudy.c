ecbeef38ff6659f325ed5c6700942dca25d940c9
enrhd24 <enrhd567@gmail.com>
2023-04-24 09:45:58 +0900

    JavaTPoint printf_scanf added

--- CStudy/CStudy.c
+++ CStudy/CStudy.c
@@ -1,6 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>    
+int main(){    
+int number;    
+printf("enter a number:");    
+scanf("%d",&number);    
+printf("cube of number is:%d\n",number*number*number);  
 
-int main(){
-    printf("Hello C Language");
-    return 0;
-}
+int x=0,y=0,result=0;  
+  
+printf("enter first number:");  
+scanf("%d",&x);  
+printf("enter second number:");  
+scanf("%d",&y);  
+  
+result=x+y;  
+printf("sum of 2 numbers:%d\n ",result);  
+
+return 0;  
+}    
