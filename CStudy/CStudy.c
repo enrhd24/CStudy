a9dff242ad326afca90072028bf0d17ed39d3f0f
enrhd24 <enrhd567@gmail.com>
2023-04-24 10:23:22 +0900

    Tyes of Variables in C Updated

--- CStudy/CStudy.c
+++ CStudy/CStudy.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 void function1();
-#define X 10; //global variable
+#define X 10; //global variable[전역 변수]
+extern int ex_x = 10 
+//external variable[외부 변수, 전역 변수]
 
 int main(){
-    static int static = 10; //static variable
+    static int static = 10; 
+    //static variable[정적 변수]
 
     return 0;
 }
 
 void function1(){
-    int x = 10; //local variable
+    int x = 10; //local variable[지역변수]
+    auto int y = 20 // automaticc variable[자동변수]
 }
