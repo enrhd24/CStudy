6bd388e01d7512616bdb8469bbcf28466bad55f0
enrhd24 <enrhd567@gmail.com>
2023-04-24 10:16:00 +0900

    Tyes of Variables in C added

--- CStudy/CStudy.c
+++ CStudy/CStudy.c
@@ -1,19 +1,13 @@
-#include<stdio.h>    
-int main(){    
-int number;    
-printf("enter a number:");    
-scanf("%d",&number);    
-printf("cube of number is:%d\n",number*number*number);  
+#include <stdio.h>
+void function1();
+#define X 10; //global variable
 
-int x=0,y=0,result=0;  
-  
-printf("enter first number:");  
-scanf("%d",&x);  
-printf("enter second number:");  
-scanf("%d",&y);  
-  
-result=x+y;  
-printf("sum of 2 numbers:%d\n ",result);  
+int main(){
+    static int static = 10; //static variable
 
-return 0;  
-}    
+    return 0;
+}
+
+void function1(){
+    int x = 10; //local variable
+}
