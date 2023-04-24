0ada6e48e50edaed43f361843ae963a3f8d7c8ba
enrhd24 <enrhd567@gmail.com>
2023-04-24 14:41:39 +0900

    Static in C Static function added static variable

--- CStudy/CStudy.c
+++ CStudy/CStudy.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <stdbool.h>
+int func();
+static void stfunc();
+/* Static global variable */
 
 int main(){
-    // Syntax
-    bool x = false;
-    if(x == true){printf("The value if x is true\n");}
-    else{printf("The value of x is FALSE\n");}
-
-    //Boolean Array
-    bool b[2] = {true, false};
-    for(int i = 0; i < 2; i++){
-        printf("%d ",b[i]);
-    }
-    printf("\n");
-    
-    // Logical Operators
-    bool f = 0;
-    bool t = 1;
-    printf("The value if x&&y is %d\n",f&&t);
-    printf("The value if x||y is %d\n",f||t);
-    printf("The value if !x is %d\n",!f);
+    /* Static local variable */
+    printf("%d ",func());
+    printf("%d\n",func());
     return 0;
+}
+
+int func(){
+    int cnt = 0; //variable initialization
+    cnt++; //increamenting counter variable
+    return cnt;
+}
+
+// static function
+static void stfunc(){
+    printf('Hello javaTpoint');
 }
