bd3754cc2f22d9045ac4a8b5c8a585068604e3bc
enrhd24 <enrhd567@gmail.com>
2023-04-25 10:48:07 +0900

    draw_ checkerboard issue Updated

--- CStudy/CStudy.c
+++ CStudy/CStudy.c
@@ -22,7 +22,7 @@ void intro(){
 	getch();
 }
 void gotoxy(short x, short y) {
-	COORD pos = { x, y };
+	COORD pos = { x-1, y-1 };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
@@ -33,15 +33,17 @@ void draw_check(int height, int width)
     unsigned char b[20];
 
 	for(i = 1; i < width; i++){
-		b[i] = 0xa0 + i;
+		b[i] = 0xa0 + i; 
+		//특수문자 넣기 위한 배열 초기화
 	}
-	printf("%c%c", a, b[3]);
 
+	printf("%c%c", a, b[3]);
 	for(i = 0; i < width; i++){
 		printf("%c%c",a,b[8]);
 	}
 	printf("%c%c",a,b[4]);
-	printf("\n");
+	printf("\n"); 
+	// 첫번째 출 입력
 
 	for(i =0; i < height; i++)
 	{
@@ -49,18 +51,18 @@ void draw_check(int height, int width)
 	for(j=0;j<20; j++)
 	{
 	printf("%c%c",a,b[11]);
-	}
-
+	} 
 	printf("%c%c",a,b[9]);
 	printf("\n");
-	}
-	printf("%c%c",a,b[6]);
+	} // 중간째 줄 모두 입력
 
+	printf("%c%c",a,b[6]);
 	for(i=0; i< width; i++){
 		printf("%c%c",a,b[10]);
 	}
 	printf("%c%c",a,b[5]);
 	printf("\n");
+	// 마지막 줄 입력
 }
 
 
@@ -68,8 +70,9 @@ void game_control(void){
 	int x = 1, y = 1, other = 0;
 	char key;
 	do{
+		 gotoxy(1,1);
 		 draw_check(12, 20);
-		 
+		 gotoxy(x,y);
 		 key = getch();
 	}while(1);
 }
