ba9f77e80b9e65d9576ceacd58ec7b833ac0f619
enrhd24 <enrhd567@gmail.com>
2023-04-25 09:43:30 +0900

    concave draw_check added

--- CStudy/CStudy.c
+++ CStudy/CStudy.c
@@ -1,20 +1,63 @@
-// https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=dgsw102&logNo=221020139485
-#include <windows.h>
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <Windows.h>
 
-void cur(short x, short y) {
+
+void intro();
+void gotoxy(short x, short y);
+void game_control(void);
+void draw_check(int c, int r);
+
+int main(void)
+{
+	intro();
+	system("cls");
+	game_control();
+	return 0;
+}
+
+void intro(){
+	printf("please push keyboard");
+	getch();
+}
+void gotoxy(short x, short y) {
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
-int main(void)
+
+void draw_check(int c, int r)
 {
-	int i;
-	for (i = 0; i < 100; i++)
-	{
-		printf("%d%%", i);
-		Sleep(10);
-		cur(0, 0);
-	}
-	printf("총 %d초가 경과되었습니다.", clock() / 1000); //필자는 10 * 100 ms 즉 1초를 기대하였다.
+        int i, j;
+        unsigned char a = 0xa6;
+        unsigned char b[12];
+        for (i = 1; i < 12; i++)
+               b[i] = 0xa0 + i;
+        printf("%c%c", a, b[3]);
+        for (i = 0; i < c - 1; i++)
+               printf("%c%c", a, b[8]);
+        printf("%c%c", a, b[4]);
+        printf("\n");
+        for (i = 0; i < r - 1; i++)
+        {
+               printf("%c%c", a, b[7]);
+               for (j = 0; j < c - 1; j++)
+                       printf("%c%c", a, b[11]);
+               printf("%c%c", a, b[9]);
+               printf("\n");
+        }
+        printf("%c%c", a, b[6]);
+        for (i = 0; i < c - 1; i++)
+               printf("%c%c", a, b[10]);
+        printf("%c%c", a, b[5]);
+        printf("\n");
+}
+
+void game_control(void){
+	int x = 1, y = 1, other = 0;
+	char key;
+	do{
+		 draw_check(18, 18);
+		 key = getch();
+	}while(1);
 }
