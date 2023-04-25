b253032cc78a1ba5e4b17e04187d2331b2311b8d
enrhd24 <enrhd567@gmail.com>
2023-04-25 16:09:48 +0900

    C_Teris added

---
+++ CStudy/CTetris.c
@@ -0,0 +1,55 @@
+#include <windows.h>
+#include <stdio.h>
+#include <conio.h>
+
+void gotoxy(int x, int y);
+void game_control(void);
+void draw_check(int height, int width);
+void intro();
+
+int main(){
+    intro();
+    system("cls");
+    game_control();
+    return 0;
+}
+
+void intro(){
+    printf("Let's start Tertris");
+    getch();
+}
+
+void gotoxy(int x, int y){
+    COORD pos = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void draw_check(int height, int width){
+    int i , j;
+
+    for(i = 0; i < height; i++){
+        for( j = 0; j < width; j++){
+            if(j == 0)printf("%c%c",0xa6,0xa2);
+            else{printf(" ");}
+            if(j == width-1)printf("%c%c",0xa6,0xa2);
+            
+        }
+        printf("\n");
+    }
+    for(i = 0; i < width; i++){
+        if(i == 0)printf("%c%c",0xa6,0xa6);
+        else{printf("%c%c",0xa6,0xa1);}
+        if(i == width-1)printf("%c%c",0xa6,0xa5);        
+    }
+}
+
+void game_control(){
+    int x = 0, y = 0;
+    char key;
+    while(1){
+    draw_check(20, 30);
+    key = getch();
+    }
+    
+
+}
