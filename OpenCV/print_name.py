1c57503605eb1c3dc93e8f55e9e79540d229e78e
enrhd24 <enrhd567@gmail.com>
2023-05-18 15:50:52 +0900

    OpenCV image cal finished

--- OpenCV/print_name.py
+++ OpenCV/print_name.py
@@ -1,18 +1,20 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("/img/opencv_logo.png")
-bgr = cv2.imread("/img/opencv_logo.png", cv2.IMREAD_COLOR)
-bgra = cv2.imread("/img/opencv_logo/png", cv2.IMREAD_UNCHANGED)
+img = cv2.imread('img/pig.jpg', cv2.IMREAD_GRAYSCALE)
+resize_img = cv2.resize(img, dsize=(300,300), interpolation=cv2.INTER_LINEAR)
 
-#print("default:",img.shape, "color:",bgr.shape, "unchanged:",bgra.shape)
+ret, th1 = cv2.threshold(img,0,255,cv2.THRESH_OTSU)
+resize_th1 = cv2.resize(th1, dsize=(300,300), interpolation=cv2.INTER_LINEAR)
 
-cv2.imshow('bgr',bgr)
-cv2.imshow('bgra',gbra)
-cv2.imshow('alpha',bgra[:,:,3])
+add = resize_img + resize_th1
+api_add = cv2.subtract(resize_img,resize_th1)
+
+cv2.imshow("resize_img", resize_img)
+cv2.imshow("th1",resize_th1)
+
+cv2.imshow("add",add)
+cv2.imshow("api_add",api_add)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-# import os
-# os.system('clear')
