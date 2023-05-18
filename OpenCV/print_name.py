fb8ae43a6ea130416958c01c0a44abb1b9984b1a
enrhd24 <enrhd567@gmail.com>
2023-05-18 14:32:37 +0900

    OpenCV learned started

--- OpenCV/print_name.py
+++ OpenCV/print_name.py
@@ -1,17 +1,18 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('img/blank_500.jpg')
+img = cv2.imread("/img/opencv_logo.png")
+bgr = cv2.imread("/img/opencv_logo.png", cv2.IMREAD_COLOR)
+bgra = cv2.imread("/img/opencv_logo/png", cv2.IMREAD_UNCHANGED)
 
-#cv2.rectangle(img, (300, 300), (100, 200), (255,0,0), 5 )
-#cv2.putText(img, "enrhd24", (140, 260), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0,0)) 
+#print("default:",img.shape, "color:",bgr.shape, "unchanged:",bgra.shape)
 
-#cv2.imshow('rectangle', img)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+cv2.imshow('bgr',bgr)
+cv2.imshow('bgra',gbra)
+cv2.imshow('alpha',bgra[:,:,3])
 
-
-#cv2.imshow('draw text', img)
-
-cv2.imshow("img", img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# import os
+# os.system('clear')
